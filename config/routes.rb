4332afe1c0f448b79299d265e95108d1ff5f201f
grandmasterspoc <dilloncortez@gmail.com>
2015-09-21 23:28:11 -0600

    routes working! :D

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :teams, :controllers => {
-      sessions: 'Teams/sessions'
-    }
+  devise_for :teams, controllers: {
+    sessions: 'teams/sessions'
+  }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
-  root 'twilio/home' => 'twilio#home'
+  root to: 'twilio#home'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
