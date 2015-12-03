50d454e84a0d66e77f3e1aa3e79784d5d6c80f09
grandmasterspoc <dilloncortez@gmail.com>
2015-12-03 13:39:11 -0700

    updated cron job to run every minute

--- config/initializers/cron_jobs.rb
+++ config/initializers/cron_jobs.rb
@@ -1,5 +1,5 @@
 Sidekiq::Cron::Job.create(name: 'SMS - worker - every Monday ', cron: '* * * * 1', klass: 'SmsWorker')
 
-Tasks.all.each do |task|
-  Sidekiq::Cron::Job.create(name: "#{task.name}", cron: "#{Task.cron_time}", klass: "CustomTask")
-end  
+
+Sidekiq::Cron::Job.create(name: "Check_", cron: "* * * * *", klass: "CustomTask")
+  
