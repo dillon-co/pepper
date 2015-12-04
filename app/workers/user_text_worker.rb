44da4378662c926467be4a011e842349cb104116
grandmasterspoc <dilloncortez@gmail.com>
2015-12-03 22:50:14 -0700

    seperated 'send_message' method into model

--- app/workers/user_text_worker.rb
+++ app/workers/user_text_worker.rb
@@ -1,37 +1,20 @@
 class CustomTask
   include Sidekiq::Worker
+  
   def perform
     User.all.each do |user|
       Task.all.each do |task|
-        t = Time.now
+        t = Time.now # t = time. I had to use t becuase time is an attribute on the Task Table
         minute, hour, month_day, month, week_day = task.cron_time.split(' ')
         message, number = task.message, user.number
-        if task.time == "Monthly"
-          if t.day == month_day.to_i
-            send_message(message, number)
-          end  
-        elsif task.time == "Weekly" 
-          if t.wday == week_day.to_i
-            send_message(message, number)
-          end  
+        if (task.time == "Monthly" && t.day == month_day.to_i) || (task.time == "Weekly" && t.wday == week_day.to_i)
+          task.send_message(message, number) 
         else
           render :error  
         end  
       end    
     end  
   end
-
-  def send_message(msg, number)
-    api = Twilio::Rest::Client.new(ENV['ACTIVITIES_ACCOUNT_SID'], ENV['ACTIVITIES_AUTH_TOKEN'])
-    messages = msg.scan(/.{1,800}/m)
-    messages.each do |message|
-      api.messages.create {
-        body: message,
-        to: number,
-        from: +18018713392
-      }
-    end
-  end  
 end  
         
 
