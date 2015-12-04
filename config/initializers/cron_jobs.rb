042c9d3e0ff20a2c70e2703748794d63c80ef5af
grandmasterspoc <dilloncortez@gmail.com>
2015-12-04 09:59:42 -0700

    added style to buttons and stuff

--- config/initializers/cron_jobs.rb
+++ config/initializers/cron_jobs.rb
@@ -1,5 +1,4 @@
 Sidekiq::Cron::Job.create(name: 'SMS - worker - every Monday ', cron: '* * * * 1', klass: 'SmsWorker')
 
-
 Sidekiq::Cron::Job.create(name: "Check_", cron: "* * * * *", klass: "CustomTask")
   
