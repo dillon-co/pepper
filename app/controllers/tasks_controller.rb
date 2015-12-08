087e04cc102294d3d3f26197ad66d8e9cb332db8
grandmasterspoc <dilloncortez@gmail.com>
2015-12-07 23:24:44 -0700

    Fixed styling on message in 'tasks_show' page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,13 +10,33 @@ class TasksController < ApplicationController
     else
       render :new
     end  
-  end  
-  
+  end 
+
+  def edit
+    @task = Task.find(params[:id]) 
+  end 
+
+  def update
+    task = Task.find(params[:id])
+    if task.update(task_params)
+      redirect_to task_path(task)
+    else
+      render :show
+    end    
+  end 
+    
   def index
     @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
