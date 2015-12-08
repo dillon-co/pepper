0e670d8d255a6482b2308692cf85d22efc2538bb
grandmasterspoc <dilloncortez@gmail.com>
2015-12-07 21:35:17 -0700

    Fixed weekly text worker

--- app/workers/weekly_reminder_worker.rb
+++ app/workers/weekly_reminder_worker.rb
@@ -1,22 +1,19 @@
 class WeeklyReminderWorker 
   include Sidekiq::Worker
   def perform
-    Team.all.each do |team|
-      if team.weekly_reminder_enabled
-        User.pair.each do |user|
+    Team.where(weekly_reminder_enabled: true).each do |team| 
+      team.users.pair.each do |user|
+        user.pair.each do |user|
           msg = user.paired_message(User.pair[User.pair.index(user).to_i-1])
           number = user.number
-            TextWorker.perform(number, msg)
+          TextWorker.perform(number, msg)
         end
         leaders_message = "#{User.pair[0].name} and #{User.pair[1].name} are planning the event next week."
         [number1, number2, number3].each do |num|
           TextWorker.perform(num, leader_message)
-        end  
-      end    
-    end  
+        end     
+      end
+    end    
   end     
 end 
 
-
-# Team.where(weekly_reminder_email: true).each do |team|
-#       team.users.pair.each do |user|
