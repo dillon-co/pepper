b4e2013b384092e5d1e2dbff447a1a618eebea0a
grandmasterspoc <dilloncortez@gmail.com>
2015-12-13 19:52:25 -0700

    Fixed weekly reminder worker

--- app/workers/weekly_reminder_worker.rb
+++ app/workers/weekly_reminder_worker.rb
@@ -1,18 +1,18 @@
 class WeeklyReminderWorker 
   include Sidekiq::Worker
   def perform
+    paired_users = []
     Team.where(weekly_reminder_enabled: true).each do |team| 
       team.users.pair.each do |user|
-        user.pair.each do |user|
-          msg = user.paired_message(User.pair[User.pair.index(user).to_i-1])
-          number = user.number
-          TextWorker.perform_async(number, msg)
-        end
-        leaders_message = "#{User.pair[0].name} and #{User.pair[1].name} are planning the event next week."
-        [number1, number2, number3].each do |num|
-          TextWorker.perform_async(num, leader_message)
-        end     
+        paired_users << user
       end
+      msg = user.paired_message(User.pair[User.pair.index(user).to_i-1])
+      number = user.number
+      TextWorker.perform_async(number, msg)
+      leaders_message = "#{paired_users[0].name} and #{paired_users[1].name} are planning the event next week."
+      [number1, number2, number3].each do |num|
+        TextWorker.perform_async(num, leader_message)
+      end     
     end    
   end     
 end 
