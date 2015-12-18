87f57c7d574fad122550c5f4b107ad5b42b0f7b3
grandmasterspoc <dilloncortez@gmail.com>
2015-12-18 15:51:54 -0700

    Fixed Sessions Controller

--- app/controllers/Teams/sessions_controller.rb
+++ app/controllers/Teams/sessions_controller.rb
@@ -1,4 +1,4 @@
-class Teams::SessionsController < Devise::SessionsController
+class Team::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
