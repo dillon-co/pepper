d7907ac121c5229e92bd03f80679fd0e6f2b9fe2
grandmasterspoc <dilloncortez@gmail.com>
2015-12-18 15:50:19 -0700

    Fixed Registrations Controller

--- app/controllers/Teams/registrations_controller.rb
+++ app/controllers/Teams/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Teams::RegistrationsController < Devise::RegistrationsController
+class Team::RegistrationsController < Devise::RegistrationsController
 # before_filter :configure_sign_up_params, only: [:create]
 # before_filter :configure_account_update_params, only: [:update]
 
