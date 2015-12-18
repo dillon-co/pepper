1dd99647614c9ff84c55d637e24eb468552a08f0
grandmasterspoc <dilloncortez@gmail.com>
2015-12-18 16:36:58 -0700

    Changed the redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,6 @@
-$redis = Redis.new(url: ENV["REDIS_URL"])
+# $redis = Redis.new(url: ENV["REDIS_URL"])
+
+if ENV["REDIS_URL"]
+    uri = URI.parse(ENV["REDIS_URL"])
+    $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
