c26d39907ec42236c08831a036e9bdad50f30ed3
grandmasterspoc <dilloncortez@gmail.com>
2015-12-18 16:44:22 -0700

    Updated redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,7 @@
 # $redis = Redis.new(url: ENV["REDIS_URL"])
 
-if ENV["REDIS_URL"]
-    uri = URI.parse(ENV["REDIS_URL"])
-    $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if ENV["REDISTOGO_URL"]
+    uri = URI.parse(ENV["REDISTOGO_URL"])
+    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    Resque.redis = REDIS
 end
