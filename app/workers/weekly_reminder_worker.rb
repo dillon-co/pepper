53ad26c97dfcae29b6e0d35ae272c33ada33a0fe
grandmasterspoc <dilloncortez@gmail.com>
2015-12-18 16:11:38 -0700

    Added a 'do' to the block

--- app/workers/weekly_reminder_worker.rb
+++ app/workers/weekly_reminder_worker.rb
@@ -2,7 +2,7 @@ class WeeklyReminderWorker
   include Sidekiq::Worker
   def perform
     paired_users = []
-    Team.where(weekly_reminder_enabled: true).each |team| 
+    Team.where(weekly_reminder_enabled: true).each do |team| 
       team.users.pair.each { |user| paired_users << user }
       msg = user.paired_message(User.pair[User.pair.index(user).to_i-1])
       number = user.number
