0bea04854ab470e1a8c68e311f5f578b104e97a9
grandmasterspoc <dilloncortez@gmail.com>
2016-05-25 12:37:33 -0600

    added rails-12factor gem

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 # $redis = Redis.new(url: ENV["REDIS_URL"])
 
-if ENV["REDISTOGO_URL"]
-    uri = URI.parse(ENV["REDISTOGO_URL"])
+if ENV["REDIS_URL"]
+    uri = URI.parse(ENV["REDIS_URL"])
     REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
     Resque.redis = REDIS
 end
